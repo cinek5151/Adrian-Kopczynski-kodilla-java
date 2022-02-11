840fa22d898610e2eed83ba051e5e7c4ea1a687a
Adrian Kopczynski <adrian.kopczynski@onet.eu>
2022-02-11 02:27:46 +0100

    6.2 First unit test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,10 +1,44 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain
 {
 
-        public static void main(String[] args) {
-            System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args)
+    {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser"))
+        {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
         }
 
+        Calculator calculator = new Calculator();
+        if(calculator.add(2,5)==(2+5))
+        {
+            System.out.println("test OK");
+        }
+        else
+        {
+            System.out.println("Error");
+        }
+
+        if(calculator.subctract(10,5)==(10-5))
+        {
+            System.out.println("test OK");
+        }
+        else
+        {
+            System.out.println("Error");
+        }
+    }
 }
+
+
+
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator
+{
+    public int add(int a, int b)
+    {
+        return a+b;
+    }
+
+    public int subctract(int a, int b)
+    {
+        return a-b;
+    }
+}
