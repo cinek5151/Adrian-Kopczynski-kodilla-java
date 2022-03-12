2546b48312ae351b38c58305cf264eccd2de131e
Adrian Kopczynski <adrian.kopczynski@onet.eu>
2022-03-12 12:19:43 +0100

    Task 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,12 +2,13 @@ package com.kodilla.stream;
 
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
-
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 
 public class StreamMain
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
         PoemBeautifier poemBeautifier = new PoemBeautifier();
         poemBeautifier.beautify("castle", String::toUpperCase);
@@ -15,5 +16,8 @@ public class StreamMain
         poemBeautifier.beautify("castle", (text) -> "sand " + text);
         poemBeautifier.beautify("castle", (text) -> text.substring(0, 4));
         poemBeautifier.beautify("castle", (text) -> text.replace("a","A"));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
